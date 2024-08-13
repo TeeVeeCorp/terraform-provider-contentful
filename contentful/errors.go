26af1e0e09aaef18aa9a0f4bcc20a450896e0a36
Colin Henry <jchenry@users.noreply.github.com>
2024-08-13 00:02:23 -0700

    bug(errors): fixed NPE in error reporting

--- contentful/errors.go
+++ contentful/errors.go
@@ -3,9 +3,10 @@ package contentful
 import (
 	"errors"
 	"fmt"
+	"strings"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/labd/contentful-go"
-	"strings"
 )
 
 func parseError(err error) diag.Diagnostics {
@@ -14,19 +15,20 @@ func parseError(err error) diag.Diagnostics {
 	}
 
 	var warnings []diag.Diagnostic
-	for _, e := range err.(contentful.ErrorResponse).Details.Errors {
-		var path []string
-		if e.Path != nil {
-			for _, p := range e.Path.([]interface{}) {
-				path = append(path, fmt.Sprintf("%v", p))
+	if err.(contentful.ErrorResponse).Details != nil {
+		for _, e := range err.(contentful.ErrorResponse).Details.Errors {
+			var path []string
+			if e.Path != nil {
+				for _, p := range e.Path.([]interface{}) {
+					path = append(path, fmt.Sprintf("%v", p))
+				}
 			}
+			warnings = append(warnings, diag.Diagnostic{
+				Severity: diag.Warning,
+				Summary:  fmt.Sprintf("%s (%s)", e.Details, strings.Join(path, ".")),
+			})
 		}
-		warnings = append(warnings, diag.Diagnostic{
-			Severity: diag.Warning,
-			Summary:  fmt.Sprintf("%s (%s)", e.Details, strings.Join(path, ".")),
-		})
 	}
-
 	return append(warnings, diag.Diagnostic{
 		Severity: diag.Error,
 		Summary:  err.(contentful.ErrorResponse).Message,
